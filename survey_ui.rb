93159ce89b6c9ad8d30c961c4b54fab61c7a4e10
Sean Morrison & Austin Steward <student@epicodus.com>
2014-08-14 11:59:55 -0700

    Add survey functionality.

--- survey_ui.rb
+++ survey_ui.rb
@@ -18,7 +18,62 @@ end
 
 def main_menu
   header
-  # add stuff here
+  puts "E > Exit"
+  puts "A > Add a survey"
+  puts "T > Take a survey"
+  puts "S > Survey stats"
+  choice = gets.chomp.upcase
+  case choice
+  when 'A'
+    add_survey
+  when 'T'
+    take_survey
+  when 'S'
+    survey_stats
+  when 'E'
+    puts "Have an excellent day!"
+    exit
+  else
+    puts 'Invalid input. Try again.'
+  end
+  main_menu
+end
+
+
+def add_survey
+  choice = ""
+  puts "Please enter name of new survey:"
+  new_survey_name = gets.chomp
+  new_survey = Survey.create({:name => new_survey_name})
+  until choice == 'N'
+    puts "Enter question:"
+    q_text = gets.chomp.capitalize
+    question = new_survey.questions.create({text: q_text})
+    a_choice = ""
+    until a_choice == "N"
+      puts "Enter a potential answer:"
+      a_text = gets.chomp.capitalize
+      answer = question.answers.create({text: a_text})
+      puts "Add another answer? (Y/N)"
+      a_choice = gets.chomp.upcase
+    end
+
+    puts "Add another question? (Y/N)"
+    choice = gets.chomp.upcase
+  end
+  puts "Survey #{new_survey.name} has been created."
+  puts "High Five! Press RETURN to continue..."
+  gets
+  main_menu
+end
+
+def take_survey
+end
+
+def survey_stats
+end
+
+def list_surveys
 end
 
 main_menu
