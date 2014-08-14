7923f091d376206fc09f6bb9e5e28401f041ad98
Sean Morrison & Austin Steward <student@epicodus.com>
2014-08-14 16:37:02 -0700

    Allow no scrubs. :stuck_out_tongue_winking_eye:

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,5 +1,19 @@
 class Survey < ActiveRecord::Base
+  validates :name, :presence => true, :length => { :minimum => 1 }
+
   has_many :questions
   has_many :answers, through: :questions
+  before_create :no_scrubs
+
+private
+
+  def no_scrubs
+    if name == "scrubs"
+      puts "I don't want no scrubs."
+      puts "Scrub is a survey that gets no love from me."
+      puts "Please enter another name."
+      name = gets.chomp
+    end
+  end
 end
 
